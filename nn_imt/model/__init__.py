13f994bb219e46847b2ee05a8dfb51758abf6bde
Chris Hokamp <chris.hokamp@gmail.com>
2016-12-11 19:04:10 +0000

    trying to get dims right in search and generate

--- nn_imt/model/__init__.py
+++ nn_imt/model/__init__.py
@@ -220,7 +220,8 @@ class PartialSequenceGenerator(BaseSequenceGenerator):
         import ipdb;ipdb.set_trace()
         # masked_probs_0 = probs_0 * (1. - model_selector)
         # masked_probs_1 = probs_1 * model_selector
-        return masked_probs_0 + masked_probs_1
+        # return masked_probs_0 + masked_probs_1
+        return probs_0 + probs_1
 
 
     @generate.delegate
